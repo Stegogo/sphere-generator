be52abfcd7e11efc1a3d9a4f31c129e879898aa8
Stegogo <lizmahrk@gmail.com>
2022-09-30 23:47:43 +0300

    restructure-code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
-int main(int argc, char const *argv[])
+#include "spheregenerator.h"
+
+int main()
 {
-    /* code */
+    // Initializing sphere parameters
+    double radius = 1.5;                 // RADIUS
+    std::vector<double> origin{0, 0, 0}; // ORIGIN
+    int rings = 100;                      // RINGS
+    int slices = 100;                     // SLICES
+
+    // Calling a function to give us an ASCII STL file
+    SphereGenerator sg(radius,rings, slices, origin);
+    sg.generateStl();
     return 0;
 }
---
+++ spheregenerator.h
@@ -0,0 +1,35 @@
+#include <vector>
+
+struct Vec
+{
+    double x, y, z;
+    int ringIndex;
+    int sliceIndex;
+};
+
+struct Triangle
+{
+    Vec A, B, C;
+    Vec normal;
+};
+
+using TriangleSoup = std::vector<Triangle>;
+
+class SphereGenerator
+{
+private:
+    double radius;
+    int rings;
+    int slices;
+
+    TriangleSoup Sphere;
+
+    std::vector<double> origin;
+    std::vector<double> getNormals(Vec A, Vec B, Vec C);
+
+public:
+    SphereGenerator();
+    SphereGenerator(double radius, int rings, int slices, std::vector<double> origin);
+    ~SphereGenerator();
+    TriangleSoup generateStl();
+};
