020ce6e67d5ba208407a8b2019cd644f67e9595a
Stegogo <lizmahrk@gmail.com>
2022-10-02 14:00:39 +0300

    fix-segfault

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include "spheregenerator.h"
+#include <string>
 
-int main()
+int main(int argc, char const *argv[])
 {
+    // spheregen -R 1 -r 10 -s 10 
     // Initializing sphere parameters
     double radius = 1.5;                 // RADIUS
     std::vector<double> origin{0, 0, 0}; // ORIGIN
     int rings = 100;                      // RINGS
     int slices = 100;                     // SLICES
+
 
     // Calling a function to give us an ASCII STL file
     SphereGenerator sg(radius,rings, slices, origin);
