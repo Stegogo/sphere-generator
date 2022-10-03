182420f5eb70b3f3fbec545c1f14bbac7ad24c85
Stegogo <lizmahrk@gmail.com>
2022-10-03 22:58:29 +0300

    add-file-name-flag

--- main.cpp
+++ main.cpp
@@ -10,16 +10,15 @@ void help()
     printf("\\__ \\ |_) | | | |  __/ | |  __/ (_| |  __/ | | |\n");
     printf("|___/ .__/|_| |_|\\___|_|  \\___|\\__, |\\___|_| |_|\n");
     printf("    |_|                        |___/            \n");
-    printf("\nSpheregen command line tool generates an ASCII STL file of a sphere");
+    printf("\nSpheregen is a command line tool which generates an ASCII STL file of a sphere");
     printf("\nwith the parameters you may specify. It may be useful if you need to");
     printf("\ngenerate a quick 3D model for some sort of test. Enjoy!\n");
     printf("\nUsage:\n");
-    printf("-R [value]\t:\tsphere radius (default: 1)\n"); 
-    printf("-r [value]\t:\tsphere horizontal segments (default: 10)\n"); 
-    printf("-s [value]\t:\tsphere verticals segments (default: 10)\n"); 
-    printf("-l\t\t:\tuse this flag to print out logs\n");
-    printf("How to read logs:\n");
-    printf("............\n");
+    printf("-R [value]\t:\tsphere radius\t\t\t(default: 1)\n"); 
+    printf("-r [value]\t:\tsphere horizontal segments\t(default: 10)\n"); 
+    printf("-s [value]\t:\tsphere vertical segments\t(default: 10)\n");
+    printf("-n [name]\t:\tspecify the filename\t\t(default: sphere)\n");
+    printf("-l\t\t:\tuse this flag to print out logs\t(default: disabled)\n");
     printf("For any further questions and bug report go to https://github.com/Stegogo/generate-stl-sphere/issues\n\n");
 
 }
@@ -27,14 +26,17 @@ void help()
 int main(int argc, char *argv[])
 {
     // Initializing sphere parameters
-    double radius = 1;                   // RADIUS
-    std::vector<double> origin{0, 0, 0}; // ORIGIN
+    int opt;
     int rings = 10;                      // RINGS
     int slices = 10;                     // SLICES
     bool logs = false;
-    int opt;
+    double radius = 1;                   // RADIUS
+    std::string name = "sphere";
+    std::string fileName = "";
+    std::vector<double> origin{0, 0, 0}; // ORIGIN
+    
 
-    while((opt = getopt(argc, argv, "R:r:s:lh")) != -1) 
+    while((opt = getopt(argc, argv, "R:r:s:n:lh")) != -1) 
     { 
         switch(opt) 
         { 
@@ -43,7 +45,8 @@ int main(int argc, char *argv[])
                 logs = true;
                 break; 
             case 'R':
-                printf("Radius: %s\n", optarg); 
+                printf("Radius: %s\n", optarg);
+                
                 radius = atof(optarg);
                 break;
             case 'r':
@@ -54,18 +57,25 @@ int main(int argc, char *argv[])
                 printf("Vertical segments: %s\n", optarg);
                 slices = atoi(optarg);
                 break;
+            case 'n':
+                printf("Filename: %s\n", optarg);
+                name.assign(optarg);
+                break;
             case 'h':
                 help();
                 return 0;
                 break;
             case '?': 
                 printf("Unknown option: %c!!\nUse -h flag for help", optopt);
+                return 0;
                 break; 
         } 
     } 
-    
+
+    fileName = name + ".stl";
+
     // Calling a function to give us an ASCII STL file
-    SphereGenerator sg(radius,rings, slices, origin, logs);
+    SphereGenerator sg(radius,rings, slices, origin, logs, fileName);
     sg.exec();
     return 0;
 }
