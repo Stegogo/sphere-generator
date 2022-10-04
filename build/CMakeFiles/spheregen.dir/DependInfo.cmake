ac84ac1d8a1bd5dceaa7cf1f168c4a0a376c8be4
Stegogo <lizmahrk@gmail.com>
2022-10-04 15:47:24 +0300

    cmake

---
+++ build/CMakeFiles/spheregen.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/maru/Work/misc/generate-stl-sphere/main.cpp" "CMakeFiles/spheregen.dir/main.cpp.o" "gcc" "CMakeFiles/spheregen.dir/main.cpp.o.d"
+  "/home/maru/Work/misc/generate-stl-sphere/spheregenerator.cpp" "CMakeFiles/spheregen.dir/spheregenerator.cpp.o" "gcc" "CMakeFiles/spheregen.dir/spheregenerator.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/spheregen.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spheregen.dir/main.cpp.o"
+  "CMakeFiles/spheregen.dir/main.cpp.o.d"
+  "CMakeFiles/spheregen.dir/spheregenerator.cpp.o"
+  "CMakeFiles/spheregen.dir/spheregenerator.cpp.o.d"
+  "bin/spheregen"
+  "bin/spheregen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spheregen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
